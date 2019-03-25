5dbe6b41bdb4a66ed2a9f679445287291b4f60a2
ECarlson <ericcarlson4@outlook.com>
2019-03-24 23:26:04 -0700

    add more stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+//global variables
+
+
+
+
+
+
+
+
+
+
+//four buttons that will have images of gemstones
+
+
+
+
+
+
+
+//need a random number generator for the crystal and starting values
+
+
+//when the player clicks on a crystal, it will give them a certian amount of points
+//The game will hide the value of the crystal at all times
+
+//player wins when number matches target, player loses when they go over
+
+
+
+
+
+
+
+
+//game restarts 
