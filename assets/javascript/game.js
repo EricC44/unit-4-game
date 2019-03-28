741a3eb7fd8a8fb769f2b0926221e34cfe91f313
ECarlson <ericcarlson4@outlook.com>
2019-03-28 02:11:45 -0700

    more javascript/jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,97 +1,55 @@
+
+//function added to make sure the code doesn't start until the page loads
 $(document).ready(function() { ;
 
 
 
 
 
-//global variables
-var wins = 0;
-var loses = 0;
-var totalNumber
-var yourNumber;
-var blueGem;
-var greenGem;
-var redGem;
-var yellowGem;
+    //global variables
+    var wins = 0;
+    var loses = 0;
+    var totalNumber
+    var yourNumber;
+    var blueGem;
+    var greenGem;
+    var redGem;
+    var yellowGem;
 
 
-$("#wins").text(wins);
-$("#wins").text(loses);
+    $("#wins").text(wins);
+    $("#loses").text(loses);
 
 
-//This is going to be picking my total number at random
+    //This is going to be picking my total number at random
 
-var totalNumber = Math.floor(Math.random() * 101 + 19)
+    var totalNumber = Math.floor(Math.random() * 101 + 19)
 
-console.log(totalNumber);
-//This should append our value to the totalNumber div
-$("#totalNumber").text(totalNumber);
-
-
-
+    console.log(totalNumber);
+    //This should append our value to the totalNumber div
+    $("#totalNumber").text(totalNumber);
 
 
 
-//This is for picking random values for each gemstone
-var blueGem = Math.floor(Math.random() * 12) + 1;
-var greenGem = Math.floor(Math.random() * 12) + 1;
-var redGem = Math.floor(Math.random() * 12) + 1;
-var yellowGem = Math.floor(Math.random() * 12) + 1;
 
-$(document).ready(function() {
-    $("#blue-gemstone").on("click", function() {
-       
-        yourNumber += blueGem;
+
+
+    //This is for picking random values for each gemstone
+    var blueGem = Math.floor(Math.random() * 12) + 1;
+    var greenGem = Math.floor(Math.random() * 12) + 1;
+    var redGem = Math.floor(Math.random() * 12) + 1;
+    var yellowGem = Math.floor(Math.random() * 12) + 1;
+    console.log(blueGem , greenGem , redGem , yellowGem);
     
-        
-    });
+    $("#blue-gem").on("click", function() {
+        yourNumber = yourNumber + blueGem;
 
-});
-
-$(document).ready(function() {
-    $("#green-gemstone").on("click", "#green-gemstone.jpg", function() {
-
-        yourNumber += greenGem;
-    });
-});
-
-$(document).ready(function() {
-    $("#red-gemstone").on("click", function() {
-
-        yourNumber += redGem;
-
-
-    });
- 
-});
-
-$(document).ready(function() {
-    $("#yellow-gemstone").on("click", function() {
-
-        yourNumber += yellowGem;
-
-
+        console.log("New yourNumber= " + yourNumber);
     });
 
 
-});
 
-function gameOver() {
 
-    if(yourNumber > totalNumber) {
-        alert("You Lose")
-        newGame = true;
-    }
-
-    if(yourNumber === totalNumber) {
-        alert("You Win")
-        newGame = true;
-    }
-
-    if(yourNumber < totalNumber) {
-        newGame = false;
-    }
-}
 
 
 
